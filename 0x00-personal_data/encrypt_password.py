070406b52809ebea67e224cf64dbedd11cdec613
Maurice <maurice.nzai@gmail.com>
2024-04-11 07:28:26 +0000

    Add Task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     Hashes a password and adds random salt.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    validates provided password against the stored hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
