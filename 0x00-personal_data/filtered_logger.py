daa1596f3a1c716f5fc00bc7dbeaf909b898ba05
Maurice <maurice.nzai@gmail.com>
2024-04-11 06:58:11 +0000

    Add Task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -45,6 +45,25 @@ def get_logger() -> logging.Logger:
     return logger
 
 
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Creates a secure connector to a database.
+    returns the connector (mysql.connector.connection.MySQLConnection object)
+    """
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "holberton")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    connection = mysql.connector.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name,
+    )
+    return connection
+
+
 class RedactingFormatter(logging.Formatter):
     """
     Redacting Formatter class
