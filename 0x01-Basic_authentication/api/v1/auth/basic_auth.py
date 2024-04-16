bae0ffb010e4a23e33719ae5db5448b730ec7494
Maurice <maurice.nzai@gmail.com>
2024-04-16 11:47:50 +0000

    Update Task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-"""Basic authentication module for the API.
 """
+Basic authentication module for the API.
+"""
+
+import re
+
+from typing import Tuple, TypeVar
+
 from .auth import Auth
 
 
@@ -8,4 +14,14 @@ class BasicAuth(Auth):
     """
     Basic authentication class.
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
