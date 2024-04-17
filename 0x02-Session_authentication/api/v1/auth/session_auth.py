c7b4707e69b13f408e9b607c27ad4ececc7eb96c
Maurice <maurice.nzai@gmail.com>
2024-04-17 12:20:23 +0000

    Add Task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Base SessionAuth class
+"""
+
+
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session Authorization protocol class
+    """
+    pass
