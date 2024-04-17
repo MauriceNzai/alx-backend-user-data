26516e2672e5cb048aa09abe1c3fb3f7467e7781
Maurice <maurice.nzai@gmail.com>
2024-04-17 12:30:16 +0000

    Add Task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,7 @@
 """
 Base SessionAuth class
 """
-
+from uuid import uuid4
 
 from .auth import Auth
 
@@ -11,4 +11,19 @@ class SessionAuth(Auth):
     """
     Session Authorization protocol class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user with given id (user_id)
+        Args:
+            user_id (str): user's user id to create a session for
+        Return:
+            None if user_id is None or not a string
+            str - Session ID for the user
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
