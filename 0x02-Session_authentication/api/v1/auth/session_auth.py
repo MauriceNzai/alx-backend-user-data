0a6604009409f39cfe93ae68f991fa094084c27b
Maurice <maurice.nzai@gmail.com>
2024-04-18 06:30:33 +0000

    Add Task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,8 @@ Base SessionAuth class
 from uuid import uuid4
 
 from .auth import Auth
+from models.user import User
+from typing import TypeVar
 
 
 class SessionAuth(Auth):
@@ -39,3 +41,16 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        Return a user instance based on a cookie value
+        Args:
+            request : request object containing cookie
+        Return:
+            User instance
+        """
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        user = User.get(user_id)
+        return user
