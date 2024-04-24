b70675c124c30cacc5d64ef348a19f9e8fe2f250
Maurice <maurice.nzai@gmail.com>
2024-04-24 13:07:35 +0000

    Update Task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,6 +52,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             if user is not None:
+
                 return bcrypt.checkpw(password.encode(
                     "utf-8"), user.hashed_password)
         except NoResultFound:
